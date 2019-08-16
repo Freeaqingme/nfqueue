9e790abdf6562506e89e9138867871189a530b3f
Reza <reza@ies-italia.it>
2019-08-16 19:38:12 +0430

    Minor changes

--- test/prepare.sh
+++ test/prepare.sh
@@ -5,6 +5,6 @@ iptables -t nat -F
 iptables -t mangle -F
 iptables -t filter -F
 
-iptables -A INPUT -j NFQUEUE --queue-balance 0:3
-iptables -A OUTPUT -j NFQUEUE --queue-balance 0:3
-iptables -A FORWARD -j NFQUEUE --queue-balance 0:3
+iptables -A INPUT -j NFQUEUE --queue-balance 0:3 --queue-cpu-fanout
+iptables -A OUTPUT -j NFQUEUE --queue-balance 0:3 --queue-cpu-fanout
+iptables -A FORWARD -j NFQUEUE --queue-balance 0:3 --queue-cpu-fanout
